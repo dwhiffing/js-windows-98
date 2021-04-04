1e0faa15569b98131835cb81472d9ea50beb6876
Daniel Whiffing <danielwhiffing@gmail.com>
2021-04-03 23:38:30 -0400

    Updates

---
+++ precache-manifest.7247ae30e4c28fba83ee18a9a24a4e37.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56987bc65ac5eb31e02874a6f5a8535",
+    "url": "/crypto-win/index.html"
+  },
+  {
+    "revision": "71bf71cdbc48ab450696",
+    "url": "/crypto-win/static/css/2.8a9348a0.chunk.css"
+  },
+  {
+    "revision": "99afb7515fb8379f8aec",
+    "url": "/crypto-win/static/css/main.ce65cedc.chunk.css"
+  },
+  {
+    "revision": "71bf71cdbc48ab450696",
+    "url": "/crypto-win/static/js/2.903312a5.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/crypto-win/static/js/2.903312a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f8744bb4875b3bb48da",
+    "url": "/crypto-win/static/js/3.f84fedad.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/crypto-win/static/js/3.f84fedad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99afb7515fb8379f8aec",
+    "url": "/crypto-win/static/js/main.bf0bb9f8.chunk.js"
+  },
+  {
+    "revision": "f3f98922de4f1ef3734a",
+    "url": "/crypto-win/static/js/runtime-main.879d1045.js"
+  },
+  {
+    "revision": "19d21185d7a1d7e01c2feb3695862ba7",
+    "url": "/crypto-win/static/media/chord.19d21185.mp3"
+  },
+  {
+    "revision": "ae7aa376f670f697a7b3eddf17ac5227",
+    "url": "/crypto-win/static/media/click.ae7aa376.mp3"
+  },
+  {
+    "revision": "a8b420d4eb4e20d931ad11b4fd5dabac",
+    "url": "/crypto-win/static/media/ding.a8b420d4.mp3"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/crypto-win/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/crypto-win/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/crypto-win/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/crypto-win/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
